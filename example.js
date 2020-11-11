2088474e8778a579cbc14642574bc31507191920
devjjinii <devjjinii@naver.com>
2020-11-11 23:02:44 +0900

    6,7,8 수정중!

--- example.js
+++ example.js
@@ -1,1 +1,73 @@
-// 06,07,08,09 example code
+// 06,07,08 example code
+
+function a() {
+    const aVariable = 'Hi';
+}
+
+function b() {
+    a();
+    console.log(aVariable);
+}
+
+// b();
+
+let myFunc = {
+    x: function() {
+        console.log('hello');
+    }
+};
+
+myFunc.x(); //hello
+
+(function(){
+    console.log('즉시 실행');
+ })();
+
+ (function(){
+    console.log('즉시 실행');
+ }());
+
+ //const ex = if(true){}
+
+function abc() {
+    const hello = 'hello';
+    console.log(hello);
+}
+
+abc()
+console.log(hello);
+
+let a = 1;
+function abc() {
+    console.log(a);
+}
+console.log(a);
+abc();
+
+let number = 2;
+
+function a() {
+    let number = 10;
+    b();
+}
+function b() {
+    console.log(number);
+}
+
+a(); // ?
+b(); // ?
+
+function outer() {
+    let x  = 1;
+
+    function inner() {
+        let y = 2;
+        console.log(x+y);
+    }
+
+    inner();
+}
+outer();
+
+function foo () {return 1, 2, 3, 4};
+console.log(foo()) //4
